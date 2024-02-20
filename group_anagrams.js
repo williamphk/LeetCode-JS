10eb46001f4d484a8346b91cf45a57b3643cccf7
William Poon <65807958+williamphk@users.noreply.github.com>
2024-02-20 13:12:37 -0500

    Create group_anagrams.js

---
+++ group_anagrams.js
@@ -0,0 +1,13 @@
+var groupAnagrams = function(strs) {
+    const map = new Map();
+
+    for (const str of strs) {
+        const sorted = str.split('').sort().join('');
+        if (!map.has(sorted)) {
+            map.set(sorted, []);
+        }
+        map.get(sorted).push(str);
+    }
+
+    return Array.from(map.values());
+};
